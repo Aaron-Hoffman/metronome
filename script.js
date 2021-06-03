c475095e59b054d8fefca661a0fdd3e1f0908fca
AaronHoffman <aahoffm@gmail.com>
2021-06-03 14:04:15 -0400

    added on/off button

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 const blinker = document.querySelector('.metronomeVisual');
-
 const input = document.querySelector('input'); 
+const button = document.querySelector('#start');
+let isOn = false;
+let milliseconds = 60000 / 120;
+let currentInterval;
+const click = new Audio('click.wav');
+click.play();
 
-let tempo = 0;
+button.addEventListener('click', () => {
+    if (button.textContent === "Start") {
+        button.textContent = "Stop";
+        isOn = true;
+        runMetronome(milliseconds);
+    } else {
+        button.textContent = "Start"
+        isOn = false;
+        clearInterval(currentInterval);
+    }
+})
+
 input.addEventListener('change', () => {
-    tempo = input.value;
-    setInterval(() => {
-        blinker.style.backgroundColor = 'blue';
-        setTimeout(() => {
-            blinker.style.backgroundColor = 'green';
-        }, 50)
-    }, tempo)
+    const tempo = input.value;
+    milliseconds = 60000 / tempo;
+
+    clearInterval(currentInterval);
+    
+    if (isOn) {
+        runMetronome(milliseconds);
+    }
 });
+
+const runMetronome = (time) => {
+    currentInterval = setInterval(() => {
+        // blinker.style.backgroundColor = 'blue';
+        console.log('click');
+        // setTimeout(() => {
+        //     blinker.style.backgroundColor = 'green';
+        // }, 20)
+    }, time)
+}
 
 
 
